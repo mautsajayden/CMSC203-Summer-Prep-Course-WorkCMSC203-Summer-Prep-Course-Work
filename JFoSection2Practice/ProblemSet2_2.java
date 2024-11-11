b412c5992712ab3ed3e60241e6e4abac30295ee5
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2024-11-11 03:16:02 -0500

    Add files via upload

---
+++ JFoSection2Practice/ProblemSet2_2.java
@@ -0,0 +1,16 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        
+    	        // ASCII Art - A Simple House
+    	        System.out.println("    ^     ");
+    	        System.out.println("   / \\   ");
+    	        System.out.println("  /   \\  ");
+    	        System.out.println(" /_____\\ ");
+    	        System.out.println("|       |");
+    	        System.out.println("|  _ _  |");
+    	        System.out.println("| |   | |");
+    	        System.out.println("|_|___|_|");
+    } 
+}

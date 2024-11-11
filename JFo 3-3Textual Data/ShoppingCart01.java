511ce8e5c472af5d4875c27123f64d90bd3add80
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2024-11-11 03:20:42 -0500

    Add files via upload

---
+++ JFo 3-3Textual Data/ShoppingCart01.java
@@ -0,0 +1,21 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        
+        String custName = "";
+        String itemDesc = "";
+        String message = ""; 
+        
+        
+        
+        // Assign the message variable 
+         custName = "Alex";
+         itemDesc = " Shirt";
+         message = " wants to purchase a "; 
+        
+        // Print and run the code
+        
+         System.out.println(custName  + message + itemDesc );
+         
+    }
+}

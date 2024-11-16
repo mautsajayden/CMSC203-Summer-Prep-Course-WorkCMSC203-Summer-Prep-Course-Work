762e642a6ffcf0179fd7dbfa41f19980b9365e18
Jayden Mautsa <mautsajayden11@gmail.com>
2024-11-16 02:02:35 -0500

    do while loop change

--- JFo 6-2 while and do-while Loops/src/SumofNums.java
+++ JFo 6-2 while and do-while Loops/src/SumofNums.java
@@ -8,11 +8,11 @@ public class SumofNums {
         System.out.print("Enter a number (-1 to quit): ");
         int number = console.nextInt();
 
-        while (number != -1) {
+        do{
             sum = sum + number;     // moved to top of loop
             System.out.print("Enter a number (-1 to quit): ");
             number = console.nextInt();
-        }
+        }while (number != -1);
 
         System.out.println("The sum is " + sum);
 

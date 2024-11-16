d8511c73f96c2d66c26ebc484a19260a6cd379f6
Jayden Mautsa <mautsajayden11@gmail.com>
2024-11-16 02:39:22 -0500

    new changes

--- JFo Section 6 Practice/src/DisplayMultiples.java
+++ JFo Section 6 Practice/src/DisplayMultiples.java
@@ -4,6 +4,16 @@ public class DisplayMultiples {
 
     public static void main(String args[]) {
        
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.println("Choose a number : ");
+    	int num = sc.nextInt();
+    	int sum=0;
+    	
+    	for(int i =1; i <=12; i++) {
+    		sum = num * i;
+    		System.out.println(num + " x " + i + " = " + sum);
+    	}
     }
 
 }

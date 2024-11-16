5754961bd78b23f277f5ed01323c44efc988e9e6
Jayden Mautsa <mautsajayden11@gmail.com>
2024-11-16 02:33:32 -0500

    n

--- JFo Section 6 Practice/src/ValidatePin.java
+++ JFo Section 6 Practice/src/ValidatePin.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class ValidatePin {
 
     public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner (System.in);
         
+    	int validpin = 1980;
+    	
+    	 int userPin = 0;
+     
+     while(userPin != validpin) {
+    	  System.out.println("Enter a pin : ");
+    	   userPin = sc.nextInt();
+     }
+     
+     System.out.println("The pin is correct : ");
     }
 }

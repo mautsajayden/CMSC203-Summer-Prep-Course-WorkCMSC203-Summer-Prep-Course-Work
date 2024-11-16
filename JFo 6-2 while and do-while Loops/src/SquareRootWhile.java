9fff11e8063f4f54428941e57f04aeca043356ce
Jayden Mautsa <mautsajayden11@gmail.com>
2024-11-16 01:57:28 -0500

    the new working file

--- JFo 6-2 while and do-while Loops/src/SquareRootWhile.java
+++ JFo 6-2 while and do-while Loops/src/SquareRootWhile.java
@@ -6,6 +6,14 @@ public class SquareRootWhile {
     System.out.print("Type a non-negative integer: ");
      Scanner console = new Scanner(System.in);
 	int number = console.nextInt();
+	
+	number = -1;
+	
+	while(number < 0) {
+		System.out.println("Type a num");
+		number = console.nextInt();
+	}
+	
    
     }
     

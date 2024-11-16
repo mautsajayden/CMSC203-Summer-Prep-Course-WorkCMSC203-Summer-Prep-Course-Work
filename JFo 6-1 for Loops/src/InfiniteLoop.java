546038875479bd5eedba7c62642ee6dd84472337
Jayden Mautsa <mautsajayden11@gmail.com>
2024-11-16 01:40:25 -0500

    First commint

---
+++ JFo 6-1 for Loops/src/InfiniteLoop.java
@@ -0,0 +1,13 @@
+public class InfiniteLoop {
+    public static void main(String[] args) {
+   
+    // The for loop will run infinite times.
+    for(int i =1; i<=5; i++){
+        System.out.println("Hello");
+    }
+     
+   
+  }
+} 
+    
+
---
+++ JFo 6-1 for Loops/src/ScopeTest.java
@@ -0,0 +1,26 @@
+public class ScopeTest {
+    
+    public static void main(String[] args) {
+          
+        for(int i1 = 64; i1 >=1; i1=i1/2 ){
+            System.out.print(i1 +" ");
+        } 
+       
+        System.out.println("");
+        for(int j = 0; j<=5; j++){
+            System.out.print(j +" ");
+        }
+        
+        System.out.println("");
+        for(int j = 5; j>=0; j--){
+            System.out.print(j +" ");
+        }
+        
+        System.out.println("");
+        for(int k = 2; k<=64; k=k*2){
+            System.out.print(k +" ");
+        }
+        
+
+    }  
+}

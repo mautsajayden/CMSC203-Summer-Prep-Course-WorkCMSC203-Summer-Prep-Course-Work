b486f428d6d57fc48c65f921b4b467fbdbbcaaa4
Jayden Mautsa <mautsajayden11@gmail.com>
2024-11-16 02:21:30 -0500

    new update change of break and continue

--- JFo 6-3 Using break and continue Statements/src/BreakContinue.java
+++ JFo 6-3 Using break and continue Statements/src/BreakContinue.java
@@ -5,13 +5,13 @@ public class BreakContinue {
                for( int i = 0 ; i < 10 ; i++ ) {
 
                      if( i % 2 == 0) { 
-                         
+                         continue;
                      }
 
                      System.out.println("The number is " + i );
 
                      if( i == 7 ) {
-                          
+                          break;
                       }
 
                }

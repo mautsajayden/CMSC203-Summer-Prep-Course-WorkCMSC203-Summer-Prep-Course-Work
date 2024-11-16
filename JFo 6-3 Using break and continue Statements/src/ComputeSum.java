4a10dc8476bc6cee3e5cc80c11d99a7fc892eaa6
Jayden Mautsa <mautsajayden11@gmail.com>
2024-11-16 02:14:44 -0500

    new update

--- JFo 6-3 Using break and continue Statements/src/ComputeSum.java
+++ JFo 6-3 Using break and continue Statements/src/ComputeSum.java
@@ -4,7 +4,25 @@ public class ComputeSum {
 
     public static void main(String[] args) {
 
-       
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int num =0;
+    	int sum = 0;
+    	
+    	for(int i =0; i < 10; i++) {
+    		if(num == 0) {
+    			break;
+    		}
+    		else 
+    		{
+    			System.out.println("Enter a number : ");
+            	num = sc.nextInt();
+            	sum +=num; 
+    		}
+    	}
+    	
+    	
+    	
 
     }
 }
